96dd1ffd217c1236cef6b64d89fec0a8127ebc6f
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2025-03-12 21:37:23 -0300

    Exercicio 1

---
+++ Exercicios/exercicios_s3_parte1/exercicios_s3_parte1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace exercicios_s3_p1
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("---Exercicio 1---");
+            Console.WriteLine("Digite um número:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número:");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"A soma desses dois valores é:{n1+n2}");
+        }
+    }
+}
