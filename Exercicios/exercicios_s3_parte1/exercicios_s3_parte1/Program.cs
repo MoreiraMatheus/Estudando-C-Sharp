9eee598a193ba3998b6da2429993a26df123d983
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2025-03-12 21:37:40 -0300

    exercicio 2

--- Exercicios/exercicios_s3_parte1/exercicios_s3_parte1/Program.cs
+++ Exercicios/exercicios_s3_parte1/exercicios_s3_parte1/Program.cs
@@ -10,6 +10,12 @@
             Console.WriteLine("Digite outro número:");
             int n2 = int.Parse(Console.ReadLine());
             Console.WriteLine($"A soma desses dois valores é:{n1+n2}");
+
+            Console.WriteLine("\n---Exercicio 2---");
+            double pi = 3.14159;
+            Console.WriteLine("Irei calcular a área de um circulo com base em um raio.\nDigite o valor do raio de um circulo:");
+            double raio = double.Parse(Console.ReadLine());
+            Console.WriteLine($"A Área do circulo será:{pi * Math.Pow(raio, 2):F4}");
         }
     }
 }
