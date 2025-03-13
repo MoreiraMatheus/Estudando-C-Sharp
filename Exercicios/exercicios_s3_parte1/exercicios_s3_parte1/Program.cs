359eabaa75aeb995cde2ebb4dc5722fd952965a0
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2025-03-12 21:45:24 -0300

    Exercicio 3

--- Exercicios/exercicios_s3_parte1/exercicios_s3_parte1/Program.cs
+++ Exercicios/exercicios_s3_parte1/exercicios_s3_parte1/Program.cs
@@ -16,6 +16,18 @@
             Console.WriteLine("Irei calcular a área de um circulo com base em um raio.\nDigite o valor do raio de um circulo:");
             double raio = double.Parse(Console.ReadLine());
             Console.WriteLine($"A Área do circulo será:{pi * Math.Pow(raio, 2):F4}");
+
+            Console.WriteLine("\n---Exercicio 3---");
+            Console.WriteLine("Digite 4 números, vou te falar a diferença do produto entre eles:");
+            Console.Write("A:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("B:");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("C:");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("D:");
+            int d = int.Parse(Console.ReadLine());
+            Console.Write($"O resultado da formula (A*B - C*D) será: {a*b-c*d}");
         }
     }
 }
