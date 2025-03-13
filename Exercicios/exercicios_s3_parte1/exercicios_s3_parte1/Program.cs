75fe93708dbffaf45f2d255ce4530809f5ba35fc
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2025-03-12 21:49:12 -0300

    Corrigindo quebra de linha

--- Exercicios/exercicios_s3_parte1/exercicios_s3_parte1/Program.cs
+++ Exercicios/exercicios_s3_parte1/exercicios_s3_parte1/Program.cs
@@ -5,15 +5,15 @@
         static void Main(String[] args)
         {
             Console.WriteLine("---Exercicio 1---");
-            Console.WriteLine("Digite um número:");
+            Console.Write("Digite um número:");
             int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite outro número:");
+            Console.Write("Digite outro número:");
             int n2 = int.Parse(Console.ReadLine());
             Console.WriteLine($"A soma desses dois valores é:{n1+n2}");
 
             Console.WriteLine("\n---Exercicio 2---");
             double pi = 3.14159;
-            Console.WriteLine("Irei calcular a área de um circulo com base em um raio.\nDigite o valor do raio de um circulo:");
+            Console.Write("Irei calcular a área de um circulo com base em um raio.\nDigite o valor do raio de um circulo:");
             double raio = double.Parse(Console.ReadLine());
             Console.WriteLine($"A Área do circulo será:{pi * Math.Pow(raio, 2):F4}");
 
@@ -27,7 +27,9 @@
             int c = int.Parse(Console.ReadLine());
             Console.Write("D:");
             int d = int.Parse(Console.ReadLine());
-            Console.Write($"O resultado da formula (A*B - C*D) será: {a*b-c*d}");
+            Console.WriteLine($"O resultado da formula (A*B - C*D) será: {a*b-c*d}");
+
+            Console.WriteLine("\n---Exercicio 4---");
         }
     }
 }
