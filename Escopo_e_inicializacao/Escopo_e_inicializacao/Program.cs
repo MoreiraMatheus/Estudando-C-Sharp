89ff3d5fb376d93cd9fec30bfbc17a351a8ea10c
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2025-04-03 21:14:22 -0300

    escopo de variaveis

---
+++ Escopo_e_inicializacao/Escopo_e_inicializacao/Program.cs
@@ -0,0 +1,13 @@
+﻿int x; //Declaração
+x = 10; //Inicialização
+Console.WriteLine(x); //Uma variável não pode ser usada se ela não for Inicializada!
+
+double preco = 101.0;
+double desconto = 0.0;
+if (preco > 100.0) {
+    desconto = preco * 0.1;
+}
+Console.WriteLine(desconto);
+//quando a variável é declarada dentro de uma estrutura essa variável "Deixa de existir" quando essa estrutura finaliza sua função.
+//É possível utilizar variáveis de um escopo maior dentro de um escopo menor (usar uma variável de fora de uma função dentro dela por exemplo).
+//Mas não é possivel utilizar uma variável de um escopo menor em um escopo maior.
